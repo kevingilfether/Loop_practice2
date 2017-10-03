76da50e0db3dbe19a029b872f22811acf358d0a5
kevingilfether <kevingilfether@gmail.com>
2017-10-03 11:30:33 -0400

    Initial commit

---
+++ Loop_practice2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
